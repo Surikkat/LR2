d325dacbb0fd48961a5fcb38e257228277105a7c
Surikkat <74894572+Surikkat@users.noreply.github.com>
2020-11-23 10:08:13 +0300

    Add files via upload

--- LR2/mainwindow.cpp
+++ LR2/mainwindow.cpp
@@ -7,7 +7,6 @@ MainWindow::MainWindow(QWidget *parent): QMainWindow(parent),ui(new Ui::MainWind
     ui->setupUi(this);
     ui->outputButton->setEnabled(false);//Делаем кнопку вывода в файл не активной по умолчанию
     ui->startButton->setEnabled(false);//Делаем кнопку старта не активной по умолчанию
-    bool sortingType = true;//Тип сортировки (по умолчанию алфавитная)
 }
 
 //Деструктор
@@ -98,18 +97,13 @@ void MainWindow::inputMap()
 reading.close();
 }
 
-
-//Сортировка словаря
-void MainWindow::sort()
-{
-}
-
 //Вывод map в файл
 void MainWindow::outputMap()
 {
     //Если файл для записи существует и открыт для записи
     if(recording.exists()&&recording.open(QIODevice::WriteOnly))
     {
+        //Если выбранна сортировка по алфавиту
         if(sortingType){
             //Для каждого элемента из map
             foreach (QString key, dictionary.keys()) {
@@ -124,24 +118,49 @@ void MainWindow::outputMap()
                 outputStream<<"\n";
                 str = "";
             }
+            //Если выбрана сортировка по частоте
         }else
         {
+            int maxVal=0;//Максимальное значение
+            //Определяем слово в словаре с максимальной частотой, перебирая весь словарь
+            foreach(QString key, dictionary.keys()){
+                if(dictionary.value(key)>maxVal){
+                    maxVal=dictionary.value(key);
+                }
+            }
 
+            //Выводим словарь на экран, начиная со слов с максимальной частотой
+            for(int i=0;i<dictionary.size();i++){
+                foreach (QString key, dictionary.keys()){
+                    if(dictionary.value(key)==maxVal){
+                        QTextStream outputStream(&recording);
+                        outputStream<<key;//Записываем ключ в файл
+                        outputStream<<" - ";//Записываем дефис
+                        //Записываем зачение в файл
+                        int value=dictionary.value(key);
+                        QString str;
+                        str.setNum(value);
+                        outputStream<<str;
+                        outputStream<<"\n";
+                        str = "";
+                    }
+                }
+                maxVal--;
+            }
         }
     }
-    recording.close();
+    recording.close();//Закрываем файл для записи
 }
 
 //Ядро программы
 void MainWindow::core()
 {
-    inputMap();
-    sort();
-    outputMap();
+    inputMap();//Заполняем QMap
+    outputMap();//Выводим QMap
 }
 
 //Обработка нажатия на кнопку старта
 void MainWindow::on_startButton_clicked()
 {
-    core();
+    core();//Вызываем ядро программы
 }
--- LR2/mainwindow.h
+++ LR2/mainwindow.h
@@ -21,23 +21,20 @@ public:
 
 private slots:
     void core();//Главный алгоритм заполнения и сортировки словаря
-    void inputMap();//ЗАполнение мапы
+    void inputMap();//Заполнение Qmap
     void outputMap();//Вывод Qmap в файл
-    void sort();//Сортировка Qmap
 
     void on_loadFile_clicked();//Обработка нажатия на кнопку загрузки файла
     void on_radioButton_alphabet_clicked();//Обработка нажатия на кнопку выбора режима сортировки (алфавитная)
     void on_radioButton_frequency_2_clicked();//Обработка нажатия на кнопку выбора режима сортировки (частотная)
     void on_outputButton_clicked();//Обработка нажатия на кнопку вывода в файл
-
-
-    void on_startButton_clicked();
+    void on_startButton_clicked();//Обработка нажатия на кнопку старта
 
 private:
     Ui::MainWindow *ui;//Главное окно
     QString fileName="";//Имя файла с текстом
     QFile reading;//Файл считывания
-    bool sortingType;//Тип сортировки (1-алфавитная    0-по частоте)
+    bool sortingType = true;//Тип сортировки (1-алфавитная    0-по частоте)
     QString fileOutputName="";//Имя файла для вывода
     QFile recording;//Файл записи
     QMap<QString,int> dictionary; //Словарь (ключ-значение)
